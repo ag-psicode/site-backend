527f3b75868299064a45bf3e65d0c7ec844fafcd
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-02-26 15:49:11 -0300

    Scaffold EstimateRequest and Question models

---
+++ server/estimates/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+QUESTION_TYPES = (
+    (1, 'Multiple choices'),
+    (2, 'Text field'),
+    (3, 'Char field'),
+    (4, 'File upload'),
+)
+
+class EstimateRequest(models.Model):
+    uuid = models.UUIDField()
+    contact_name = models.CharField(max_length=200)
+    contact_email = models.CharField(max_length=200)
+    contact_phone = models.CharField(max_length=20)
+
+    read = models.BooleanField(default=False)
+    replied = models.BooleanField(default=False)
+
+class Question(models.Model):
+    title = models.CharField(max_length=600)
+    description = models.TextField()
+    question_type = models.IntegerField(choices=QUESTION_TYPES)
