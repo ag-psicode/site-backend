87a34b6b8d94502f47a1c2e3ddb32ce5a60abf49
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-02-26 16:45:26 -0300

    Small refactor on User model

--- server/core/models.py
+++ server/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [] # removes email from REQUIRED_FIELDS
     email = models.EmailField('email address', unique=True) # changes email to unique and blank to false
+
+    REQUIRED_FIELDS = [] # removes email from REQUIRED_FIELDS
+    USERNAME_FIELD = 'email'
