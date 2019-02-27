6ecd0d7acb093d050a4d344e65b670706a4c04f1
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-02-27 08:47:05 -0300

    Enable QuestionChoice admin

--- server/estimates/admin.py
+++ server/estimates/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from estimates.models import EstimateRequest, Question
+from estimates.models import EstimateRequest, Question, QuestionChoice
 
 class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+class QuestionChoiceAdmin(admin.ModelAdmin):
     pass
 
 class EstimateRequestAdmin(admin.ModelAdmin):
@@ -9,4 +12,4 @@ class EstimateRequestAdmin(admin.ModelAdmin):
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(EstimateRequest, EstimateRequestAdmin)
-admin.site.register(Question, QuestionChoice)
+admin.site.register(QuestionChoice, QuestionChoiceAdmin)
